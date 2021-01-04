755b0f9fe5e630ca058470f01cbc0f070afddef2
Jonathanh0 <64462104+Jonathanh0@users.noreply.github.com>
2021-01-04 01:19:36 +0100

    archive page - api implementation

--- app/src/main/java/com/example/gachamon/main/archive.java
+++ app/src/main/java/com/example/gachamon/main/archive.java
@@ -1,16 +1,80 @@
 package com.example.gachamon.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.gachamon.R;
+import com.example.gachamon.models.ListPokemonAdapter;
+import com.example.gachamon.models.PokeRequest;
+import com.example.gachamon.models.Pokemon;
+import com.example.gachamon.pokeapi.PokeapiService;
+
+import java.util.ArrayList;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class archive extends AppCompatActivity {
+    private static final String TAG = "POKEDEX";
+    private Retrofit retrofit;
+    private ListPokemonAdapter listPokemonAdapter;
+    private RecyclerView recyclerView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archive);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        listPokemonAdapter = new ListPokemonAdapter(this);
+        recyclerView.setAdapter(listPokemonAdapter);
+        recyclerView.setHasFixedSize(true);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        fetchPokemon();
+
+
+    }
+    public void fetchPokemon() {
+        PokeapiService service = retrofit.create(PokeapiService.class);
+        Call<PokeRequest> pokeResquestCall = service.PokeList();
+
+        pokeResquestCall.enqueue(new Callback<PokeRequest>() {
+            @Override
+            public void onResponse(Call<PokeRequest> call, Response<PokeRequest> response) {
+                if (response.isSuccessful()) {
+                    PokeRequest pokeRequest = response.body();
+                    ArrayList<Pokemon> pokeList = pokeRequest.getResults();
+                    listPokemonAdapter.addListPokemon(pokeList);
+
+
+                } else {
+                    Log.e("Pokedex", "onResponse" + response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PokeRequest> call, Throwable t) {
+                Log.e("Pokedex", "onFailure" + t.getMessage());
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/gachamon/models/ListPokemonAdapter.java
@@ -0,0 +1,67 @@
+package com.example.gachamon.models;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.gachamon.R;
+
+import java.util.ArrayList;
+
+public class ListPokemonAdapter extends RecyclerView.Adapter<ListPokemonAdapter.ViewHoler> {
+
+    private ArrayList<Pokemon> dataset;
+    private Context context;
+
+    public ListPokemonAdapter(Context context) {
+        this.context = context;
+        dataset = new ArrayList<>();
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_archive_pokemon,parent,false);
+        return new ViewHoler(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHoler holder, int position) {
+        Pokemon p = dataset.get(position);
+        holder.PokeTextview.setText(p.getName());
+
+        Glide.with(context)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+ p.getNumber() +".png")
+                .centerCrop()
+                .into(holder.PokeImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+    public void addListPokemon(ArrayList<Pokemon> pokeList) {
+        dataset.addAll(pokeList);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHoler extends RecyclerView.ViewHolder {
+        private TextView PokeTextview;
+        private ImageView PokeImage;
+        public ViewHoler(View itemView) {
+            super(itemView);
+
+            PokeTextview = (TextView) itemView.findViewById(R.id.pokeTextView1);
+            PokeImage = (ImageView) itemView.findViewById(R.id.pokeImage1);
+        }
+    }
+}
